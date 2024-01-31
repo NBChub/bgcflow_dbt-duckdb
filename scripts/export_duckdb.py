e27eb2d1f2ec49282bdc4077dda33f3053cd36ca
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2024-01-31 21:42:08 +0000

    feat: add options to export as csv or parquet

--- scripts/export_duckdb.py
+++ scripts/export_duckdb.py
@@ -9,19 +9,20 @@ logging.basicConfig(
 )
 
 
-def export_database(database_filename, export_directory):
+def export_database(database_filename, export_directory, format):
     """
     Export a DuckDB database to a specified directory.
 
     Args:
     database_filename (str): The filename of the DuckDB database to export.
     export_directory (str): The directory to save the exported database.
+    format (str): The format to export the database in.
     """
     logging.info(f"Connecting to database: {database_filename}")
     conn = duckdb.connect(database_filename)
 
     logging.info(f"Exporting database to directory: {export_directory}")
-    conn.execute(f"EXPORT DATABASE '{export_directory}' (FORMAT PARQUET)")
+    conn.execute(f"EXPORT DATABASE '{export_directory}' (FORMAT {format.upper()})")
 
     logging.info("Database export completed successfully")
 
@@ -38,6 +39,12 @@ if __name__ == "__main__":
         help="The directory to save the exported database.",
         default="./exported_database",
     )
+    parser.add_argument(
+        "--format",
+        help="The format to export the database in.",
+        choices=["parquet", "csv"],
+        default="parquet",
+    )
     args = parser.parse_args()
 
-    export_database(args.database_filename, args.export_directory)
+    export_database(args.database_filename, args.export_directory, args.format)
